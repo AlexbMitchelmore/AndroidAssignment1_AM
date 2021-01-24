ccd5f3ff2c168ca874b6006a4fc079634326c737
AlexbMitchelmore <45170294+AlexbMitchelmore@users.noreply.github.com>
2021-01-24 16:04:05 -0400

    commit

---
+++ asn1/app/src/main/java/com/example/asn1_am/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.example.asn1_am
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+
+    }
+}
